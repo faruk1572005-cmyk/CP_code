ef10d0f608a31af2617da7e8ed00fde47bb88bda
MD Faruk Hossain <faruk1572005@gmail.com>
2026-03-05 20:50:52 +0600

    remove space

---
+++ 0011.cpp
@@ -0,0 +1,42 @@
+
+/*    
+
+
+       ████████████████████████████████████████████████████
+       ██               Md.Faruk Hossain                 ██
+       ██    ███████╗ █████╗ ██████╗ ██╗   ██╗██╗  ██╗   ██
+       ██    ██╔════╝██╔══██╗██╔══██╗██║   ██║██║ ██╔╝   ██
+       ██    █████╗  ███████║██████╔╝██║   ██║█████╔╝    ██
+       ██    ██╔══╝  ██╔══██║██╔══██╗██║   ██║██╔═██╗    ██
+       ██    ██║     ██║  ██║██║  ██║╚██████╔╝██║  ██╗   ██
+       ██    ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝   ██
+       ████████████████████████████████████████████████████
+
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define nl "\n"
+#define nf cout<<"\n"
+#define int long long
+#define cy cout << "YES\n"
+#define cn cout << "NO\n"
+#define fr(N)for(int i=0;i<N;i++)
+#define frg(x,a) for(auto x:a)
+#define vll vector<long long>
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+
+#define fastio ios :: sync_with_stdio(false);cin.tie(0);cout.tie(0)
+
+void solve(){
+    cout<<0<<nl;
+}
+int32_t main() {
+    fastio;
+    int t=1;
+    while(t--)solve();
+    return 0;
+}
